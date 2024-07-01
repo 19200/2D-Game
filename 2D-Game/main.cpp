853e806650b5047d830611576ae6add61a76fb7a
Jon20002179 <101559026+Jon20002179@users.noreply.github.com>
2024-07-01 17:59:06 +0100

    Game Class added, screen can now be resized when needed

--- 2D-Game/main.cpp
+++ 2D-Game/main.cpp
@@ -1,16 +1,20 @@
 #include "SDL.h"
+#include "Game.h"
+
+Game* game = nullptr;
 
 int main(int argc, char* argv[])
 {
-    SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window* window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    game = new Game();
+    game->init("Godzilla Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
-    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+    while (game->running()) {
+        game->handleEvents();
+        game->update();
+        game->render();
+    }
 
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
-    SDL_Delay(300000);
+    game->clean();
 
     return 0;
 }
