c9f29841b9c2350306c7f825adcc052e8e4f9661
Jon20002179 <101559026+Jon20002179@users.noreply.github.com>
2024-07-02 16:23:35 +0100

    Game now runs at a constant 60fps

--- 2D-Game/Game.cpp
+++ 2D-Game/Game.cpp
@@ -1,6 +1,7 @@
 #include "Game.h"
 
 SDL_Texture *playerTex;
+SDL_Rect srcR, destR;
 
 Game::Game() {}
 
@@ -49,12 +50,13 @@ void Game::handleEvents() {
 }
 
 void Game::update() {
-
+	destR.h = 64;
+	destR.w = 75;
 }
 
 void Game::render() {
 	SDL_RenderClear(renderer);
-	SDL_RenderCopy(renderer, playerTex, NULL, NULL);
+	SDL_RenderCopy(renderer, playerTex, NULL, &destR);
 	SDL_RenderPresent(renderer);
 }
 
